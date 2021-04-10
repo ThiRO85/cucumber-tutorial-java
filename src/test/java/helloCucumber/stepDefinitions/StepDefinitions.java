58f376ab12585b1af24249e52630425eeb5e5feb
ThiRO85 <thgr85@gmail.com>
2021-04-10 15:14:10 -0300

    JUnit dependency added and new methods

--- src/test/java/helloCucumber/stepDefinitions/StepDefinitions.java
+++ src/test/java/helloCucumber/stepDefinitions/StepDefinitions.java
@@ -3,21 +3,31 @@ package helloCucumber.stepDefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+class IsItFriday {
+    static String isItFriday(String today) {
+        return null;
+    }
+}
 
 public class StepDefinitions {
 
+    private String today;
+    private String actualAnswer;
+
     @Given("today is Sunday")
     public void today_is_Sunday() {
-        System.out.println("Today is Sunday");
+        today = "Sunday";
     }
 
     @When("I ask whether it's Friday yet")
     public void i_ask_whether_it_s_Friday_yet() {
-        System.out.println("I ask whether it's Friday yet");
+        actualAnswer = IsItFriday.isItFriday(today);
     }
 
     @Then("I should be told {string}")
-    public void i_should_be_told(String string) {
-        System.out.println("I should be told 'Nope'");
+    public void i_should_be_told(String expectedAnswer) {
+        Assert.assertEquals(expectedAnswer, actualAnswer);
     }
 }

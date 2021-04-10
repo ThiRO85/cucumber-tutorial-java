dc20fb463991ecccf950d7136e91ee6fa4fa5fd6
ThiRO85 <thgr85@gmail.com>
2021-04-10 14:34:54 -0300

    The simplest way to pass a Cucumber test

---
+++ src/test/java/helloCucumber/StepDefinitions.java
@@ -0,0 +1,23 @@
+package helloCucumber;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefinitions {
+
+    @Given("today is Sunday")
+    public void today_is_Sunday() {
+        System.out.println("Today is Sunday");
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_Friday_yet() {
+        System.out.println("I ask whether it's Friday yet");
+    }
+
+    @Then("I should be told {string}")
+    public void i_should_be_told(String string) {
+        System.out.println("I should be told 'Nope'");
+    }
+}
